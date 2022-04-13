4d0f9f694a92acc4f3ff5aac84a86edbf5906caa
MilesAngels <mangels@alphaworks.tech>
2022-04-13 16:28:12 -0700

    adding page.php and single.php

--- themes/fictional-university-theme/page.php
+++ themes/fictional-university-theme/page.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
 
     // Iterating through posts
     while(have_posts()) {
         the_post(); ?>
+        <h1>This is a page Not a POST</h1>
         <h2>
-            <a href="<?php the_permalink() ?>">
-            <?php the_title();?></a>
+            <?php the_title();?>
         </h2>
         <?php the_content(); ?>
         <hr>
--- themes/fictional-university-theme/single.php
+++ themes/fictional-university-theme/single.php
@@ -1,11 +1,11 @@
-<?php
+<?php 
+
 
     // Iterating through posts
     while(have_posts()) {
         the_post(); ?>
         <h2>
-            <a href="<?php the_permalink() ?>">
-            <?php the_title();?></a>
+            <?php the_title();?>
         </h2>
         <?php the_content(); ?>
         <hr>
