8fe26089b30c90f5bbd0dc073402d4ff55f53270
MilesAngels <mangels@alphaworks.tech>
2022-04-14 09:07:10 -0700

    fix error

--- themes/fictional-university-theme/footer.php
+++ themes/fictional-university-theme/footer.php
@@ -1,1 +1,5 @@
-<h3>I am a footer</h3>
+<h3>I am a footer.php</h3>
+
+<?php wp_footer(); ?>
+</body>
+</html>
--- themes/fictional-university-theme/functions.php
+++ themes/fictional-university-theme/functions.php
@@ -4,5 +4,6 @@
         wp_enqueue_style('university_main_styles', get_stylesheet_uri());
     }
 
+    //before everything is loaded into the page, call this function
     add_action('wp_enqueue_scripts', 'university_files');
 ?>
